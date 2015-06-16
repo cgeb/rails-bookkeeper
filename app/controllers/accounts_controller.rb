f98c223c5509ce52180db668950953d64971a554
chris-yukari <chris-yukari@kikuchiyukaris-MacBook-Pro.local>
2015-06-16 23:24:45 +0900

    Create editing for accounts and minor style changes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -22,15 +22,26 @@ class AccountsController < ApplicationController
       flash["notice"] = "Account was created!"
       redirect_to chartofaccounts_path
     else
+      @user = current_user
+      @classifications = Classification.all
       render 'new'
     end
   end
 
   def edit
-
+    @account = Account.find(params[:id])
+    @user = current_user
   end
 
   def update
+    @account = Account.find(params[:id])
+    @user = current_user
 
+    if @account.update(params.require(:account).permit(:name))
+      flash["notice"] = "This account has been updated."
+      redirect_to user_account_path(current_user, @account)
+    else
+      render 'edit'
+    end
   end
 end
